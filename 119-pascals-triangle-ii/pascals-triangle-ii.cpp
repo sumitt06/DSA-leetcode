4212ca3015b6ae1421dbf5b74dcc7b605a1c0d5a
sumitt06 <namdev27sumit@gmail.com>
2026-06-01 17:05:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (94.33%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     vector<int> getRow(int rowIndex) {
     vector<int> ans;
-    for(int c = 0; c <= rowIndex; c++) {
-            long long result = 1;
-        for(int i = 0; i < c; i++) {
-         result = result * (rowIndex - i);
-         result = result / (i + 1);
+    long long result = 1;
+    ans.push_back(result);
+        for(int i = 1; i <= rowIndex; i++) {
+         result = result * (rowIndex - i + 1);
+         result = result / (i);
+         ans.push_back(result);
          }
-            ans.push_back(result);
-       }
         return ans;
     }
 };
