a45e2d392c00317934180fc1e9fe3671452f0d24
sumitt06 <namdev27sumit@gmail.com>
2026-06-13 21:33:58 +0530

    Time: 7 ms (39.88%) | Memory: 23.7 MB (81.77%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,28 +1,48 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+
+        // brute solution
+
+
+        // int n = intervals.size();
+        // sort(intervals.begin() , intervals.end());      // TC = O(n log n)
+        // vector<vector<int>> ans;                        // SC = O(n)
+        // for(int i = 0 ; i < n ; i++) {                  // TC = O(n^2)
+        //     int start = intervals[i][0];
+        //     int end = intervals[i][1];
+        //     if(!ans.empty() && end <= ans.back()[1]) {
+        //         continue;
+        //     }
+        //     else{
+        //         for(int j = i + 1 ; j < n ; j++) {
+        //             if(intervals[j][0] <= end) {               
+        //                 end = max (end , intervals[j][1]);
+        //             }
+        //             else{
+        //                 break;
+        //             }
+        //         }
+        //     }
+        //     ans.push_back({start , end});
+        // }
+        // return ans;
+
+
+        // optimal solution
+
         int n = intervals.size();
         sort(intervals.begin() , intervals.end());
         vector<vector<int>> ans;
         for(int i = 0 ; i < n ; i++) {
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            if(!ans.empty() && end <= ans.back()[1]) {
-                continue;
+            if(ans.empty() || intervals[i][0] > ans.back()[1]) {
+                ans.push_back(intervals[i]);
             }
             else{
-                for(int j = i + 1 ; j < n ; j++) {
-                    if(intervals[j][0] <= end) {
-                        end = max (end , intervals[j][1]);
-                    }
-                    else{
-                        break;
-                    }
-                }
+                ans.back()[1] = max(intervals[i][1] , ans.back()[1]);
             }
-            ans.push_back({start , end});
         }
         return ans;
-        
     }
 };
