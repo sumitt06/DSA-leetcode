83322d307f99ca468f4362f989e5ce94545dbfe0
sumitt06 <namdev27sumit@gmail.com>
2026-06-19 16:22:17 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (44.68%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        vector<int> altitude;
+
         int n = gain.size();
         int startingAltitude = 0;
-        altitude.push_back(startingAltitude);
-        for(int i = 0 ; i < n ; i++) {
+        int highest = 0;
+
+        for (int i = 0; i < n; i++) {
             startingAltitude += gain[i];
-            altitude.push_back(startingAltitude);
+            highest = max(highest, startingAltitude);
         }
-        int m = altitude.size();
-        int highest = INT_MIN;
-        for(int i = 0 ; i < m ; i++) {
-            highest = max(highest , altitude[i]);
-        }
+
         return highest;
     }
 };
