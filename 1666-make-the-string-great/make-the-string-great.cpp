9afb1f51e0b97a1959a364ce6a336b256c6f7d41
sumitt06 <namdev27sumit@gmail.com>
2026-06-20 15:57:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (46.70%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char> st;
+        
+        for(char ch : s) {
+            if(!st.empty() && abs(st.top() - ch) == 32) {
+                st.pop();
+            }
+            else{
+                st.push(ch);
+            }
+        }
+        string ans;
+        while(!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin() , ans.end());
+        return ans;
+    }
+};
