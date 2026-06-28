d5be909be2614e9d8bfe6482e51e6fddc4635b4e
sumitt06 <namdev27sumit@gmail.com>
2026-06-28 23:17:40 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (96.92%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-        int n = s.size();
-        int cnt = 0;
-        int segment = 0;
-        for(int i = 0 ; i < n ; i++) {
-            if(s[i] == '1') {
-                segment = 1 ;
-            }
-            else{
-                cnt += segment;
-                segment = 0;
+        // int n = s.size();
+        // int cnt = 0;
+        // int segment = 0;
+        // for(int i = 0 ; i < n ; i++) {
+        //     if(s[i] == '1') {
+        //         segment = 1 ;
+        //     }
+        //     else{
+        //         cnt += segment;
+        //         segment = 0;
+        //     }
+        // }
+        // if(s[n-1] == '1') {
+        //     cnt++;
+        // }
+        // if(cnt == 1) {
+        //     return true;
+        // }
+        // return false; /
+
+        bool seenZero = false;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '0') {
+                seenZero = true;
+            } else if (seenZero == true) {
+                return false;
             }
         }
-        if(s[n-1] == '1') {
-            cnt++;
-        }
-        if(cnt == 1) {
-            return true;
-        }   
-        return false; 
+        return true;
     }
 };
