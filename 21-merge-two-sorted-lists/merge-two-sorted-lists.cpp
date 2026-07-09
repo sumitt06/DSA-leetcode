a964c0079e0f4258f2a4cc1f645c8c06aba20906
sumitt06 <namdev27sumit@gmail.com>
2026-07-09 22:33:14 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (8.97%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,6 +11,29 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        vector<int> arr;
+        while(list1 != NULL) {
+            arr.push_back(list1->val);
+            list1 = list1->next;
+        }
+        while(list2 != NULL) {
+            arr.push_back(list2->val);
+            list2 = list2->next;
+        }
+
+        sort(arr.begin() , arr.end());
+
+        ListNode* temp = new ListNode(-101);
+        ListNode* ans = temp;
+
+        for(int i = 0 ; i < arr.size() ; i++) {
+            temp->next = new ListNode(arr[i]);
+            temp = temp->next;
+        }
+        return ans -> next;
+
+
+
         // vector<int> arr;
         // while (list1 != NULL && list2 != NULL) {
         //     if (list1->val < list2->val) {
@@ -46,37 +69,40 @@ public:
 
 
 
-        ListNode* temp = new ListNode(-101);
-        ListNode* ans = temp;
-        while (list1 != NULL && list2 != NULL) {
-            if (list1->val < list2->val) {
-                temp->next = new ListNode(list1->val);
-                temp = temp->next;
-                list1 = list1->next;
-            } else if (list2->val < list1->val) {
-                temp->next = new ListNode(list2->val);
-                temp = temp->next;
-                list2 = list2->next;
-            } else {
-                temp->next = new ListNode(list1->val);
-                temp = temp->next;
-                list1 = list1->next;
+        // ListNode* temp = new ListNode(-101);
+        // ListNode* ans = temp;
+        // while (list1 != NULL && list2 != NULL) {
+        //     if (list1->val < list2->val) {
+        //         temp->next = new ListNode(list1->val);
+        //         temp = temp->next;
+        //         list1 = list1->next;
+        //     } else if (list2->val < list1->val) {
+        //         temp->next = new ListNode(list2->val);
+        //         temp = temp->next;
+        //         list2 = list2->next;
+        //     } else {
+        //         temp->next = new ListNode(list1->val);
+        //         temp = temp->next;
+        //         list1 = list1->next;
 
-                temp->next = new ListNode(list2->val);
-                temp = temp->next;
-                list2 = list2->next;
-            }
-        }
-        while(list1 != NULL) {
-            temp->next = new ListNode(list1->val);
-            temp = temp->next;
-            list1 = list1->next;
-        }
-        while(list2 != NULL) {
-            temp->next = new ListNode(list2->val);
-            temp = temp->next;
-            list2 = list2->next;
-        }
-        return ans -> next;
+        //         temp->next = new ListNode(list2->val);
+        //         temp = temp->next;
+        //         list2 = list2->next;
+        //     }
+        // }
+        // while(list1 != NULL) {
+        //     temp->next = new ListNode(list1->val);
+        //     temp = temp->next;
+        //     list1 = list1->next;
+        // }
+        // while(list2 != NULL) {
+        //     temp->next = new ListNode(list2->val);
+        //     temp = temp->next;
+        //     list2 = list2->next;
+        // }
+        // return ans -> next;
+
+
+
     }
 };
