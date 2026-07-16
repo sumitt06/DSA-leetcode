ffe10fd56ee84efbc2c7d07898bdfda85b15f9d2
sumitt06 <namdev27sumit@gmail.com>
2026-07-16 18:32:58 +0530

    Time: 3 ms (33.49%) | Memory: 69.4 MB (94.27%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -11,12 +11,12 @@ public:
                 cnt++;
             }
             if(cnt > k) {
-                while(cnt > k) {
+                
                     if(nums[i] == 0) {
                         cnt--;
                     }
                     i++;
-                }
+                
             }
             if(cnt <= k) {
                 maxi = max(maxi , j - i + 1);
