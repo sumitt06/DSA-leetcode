8d160805b33cf219bbb4a365eb77c6f1a30e02c9
sumitt06 <namdev27sumit@gmail.com>
2026-07-20 23:18:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (56.77%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        long long sum = 0 ;
-        for(int i = 1 ; i <= num/2 ; i++) {
-            if(num % i == 0) {
-                sum += i ;
+        // long long sum = 0 ;
+        // for(int i = 1 ; i <= num/2 ; i++) {
+        //     if(num % i == 0) {
+        //         sum += i ;
+        //     }
+        // }
+        // return sum == num ;
+
+        if (num == 1) {
+            return false;
+        }
+        int sum = 1;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i != num / i)
+                    sum += num / i;
             }
         }
-        return sum == num ;
+        return sum == num;
     }
 };
