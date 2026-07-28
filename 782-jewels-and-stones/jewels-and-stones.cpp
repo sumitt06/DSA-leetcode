9ae6520279b0d5be7cfcdf5de75479a56fcef2a1
sumitt06 <namdev27sumit@gmail.com>
2026-07-28 23:45:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (64.61%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        unordered_map<char,int> mpp;
-        for(int i = 0 ; i < stones.size() ; i++) {
-            mpp[stones[i]]++;
+        // unordered_map<char,int> mpp;
+        // for(int i = 0 ; i < stones.size() ; i++) {
+        //     mpp[stones[i]]++;
+        // }
+        // int ans = 0 ;
+        // for(int i = 0 ; i < jewels.size() ; i++) {
+        //     ans += mpp[jewels[i]];
+        // }
+        // return ans ;
+
+        
+        bool isJewel[128] = {false};
+        for(char ch : jewels){
+            isJewel[ch] = true;
         }
-        int ans = 0 ;
-        for(int i = 0 ; i < jewels.size() ; i++) {
-            ans += mpp[jewels[i]];
+        int cnt = 0;
+        for(char ch : stones){
+            if(isJewel[ch] == 1){
+                cnt++;
+            }
         }
-        return ans ;
+        return cnt;
     }
 };
