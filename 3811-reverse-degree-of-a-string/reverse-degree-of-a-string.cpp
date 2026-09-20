027dd16b6607f3c42ea9fbc1cfee07197223ac97
sumitt06 <namdev27sumit@gmail.com>
2026-09-20 18:33:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (47.88%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-        string alphabets = "zyxwvutsrqponmlkjihgfedcba";
-        unordered_map<char , int>mpp;
-        for(int i = 0 ; i < 26 ; i++) {
-            mpp[alphabets[i]] = i + 1;
-        }
+        // string alphabets = "zyxwvutsrqponmlkjihgfedcba";
+        // unordered_map<char , int>mpp;
+        // for(int i = 0 ; i < 26 ; i++) {
+        //     mpp[alphabets[i]] = i + 1;
+        // }
+        // int sum = 0;
+        // for(int i = 0 ; i < s.size() ; i++) {
+        //     int product = mpp[s[i]] * (i + 1);
+        //     sum += product;
+        // }
+        // return sum;
+
         int sum = 0;
         for(int i = 0 ; i < s.size() ; i++) {
-            int product = mpp[s[i]] * (i + 1);
-            sum += product;
+            int reverseVal = 'z' - s[i] + 1;
+            sum += reverseVal * (i + 1);
         }
         return sum;
     }
